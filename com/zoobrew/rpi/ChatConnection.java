3d1dc39358adb7934426bddc7b53a5eda6d16c80
Jared Corso <jaredcorso@gmail.com>
2014-04-02 03:25:01 -0400

    added mutithreading to support mutiple users

--- com/zoobrew/rpi/ChatConnection.java
+++ com/zoobrew/rpi/ChatConnection.java
@@ -1,14 +1,41 @@
 package com.zoobrew.rpi;
 
-/* A single connection by a user to the server */
-public class ChatConnection {
-	
-	public enum ConnectionType{
-		TCP,UDP;
-	};
-	/* Case-insensitive, must be downcased */
-	public String userName;
-	public int ip;
-	
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
 
+/* A single connection by a user to the server */
+public class ChatConnection implements Runnable{
+
+    public enum ConnectionType{
+        TCP,UDP;
+    };
+    /* Case-insensitive, must be downcased */
+    public String userName;
+    public int ip;
+    Socket clientSocket;
+
+    ChatConnection(Socket socket){
+        clientSocket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        System.out.println("Accepted client socket");
+
+        String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                out.println(inputLine);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException caught while writing to client");
+            System.out.println(e.getMessage());
+        }
+
+    }
 }
