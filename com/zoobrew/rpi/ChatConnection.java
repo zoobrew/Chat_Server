3e4e382946662c86a286aad0519ac7780052710e
Jared Corso <jaredcorso@gmail.com>
2014-04-04 14:50:05 -0400

    Add logging in with ME IS

--- com/zoobrew/rpi/ChatConnection.java
+++ com/zoobrew/rpi/ChatConnection.java
@@ -9,13 +9,18 @@ import java.net.Socket;
 /* A single connection by a user to the server */
 public class ChatConnection implements Runnable{
 
-    public enum ConnectionType{
+    private String LOGIN = "ME IS ";
+    private String SEND_MESSAGE = "SEND ";
+    private String SEND_ALL = "BROADCAST ";
+    private String HERE = "WHO HERE";
+    private String EXIT = "LOGOUT";
+
+	public enum ConnectionType{
         TCP,UDP;
     };
     /* Case-insensitive, must be downcased */
     public String userName;
-    public int ip;
-    Socket clientSocket;
+    public Socket clientSocket;
 
     ChatConnection(Socket socket){
         clientSocket = socket;
@@ -26,16 +31,46 @@ public class ChatConnection implements Runnable{
         try {
         PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-        System.out.println("Accepted client socket");
+        System.out.println("Accepted client socket from " + clientSocket.getRemoteSocketAddress());
 
         String inputLine;
+        String input;
             while ((inputLine = in.readLine()) != null) {
-                out.println(inputLine);
+            	input = inputLine.trim();
+            	parseMessage(inputLine, out);
             }
         } catch (IOException e) {
             System.out.println("IOException caught while writing to client");
             System.out.println(e.getMessage());
         }
 
+    }
+    
+    private void parseMessage(String input, PrintWriter write){
+    	if(input.startsWith(LOGIN)){
+    		System.out.println("Logging in");
+    		String message = input.substring(LOGIN.length());
+    		String username;
+    		if (message.indexOf('\n') > -1){
+    			userName = message.substring(0, message.indexOf(' '));
+    		}else if (!(message.indexOf(' ') < 0)){
+    			userName = message.substring(0, message.indexOf(' '));
+    		} else {
+    			userName = message;
+    		}
+    		write.println("Username is: " + userName);
+    			
+    		//String username = message.substring(0, message.indexOf(' ')
+    	} else if (input.startsWith(SEND_MESSAGE)){
+    		
+    	} else if (input.startsWith(SEND_ALL)){
+    		
+    	} else if (input.startsWith(HERE)){
+    		
+    	} else if (input.startsWith(EXIT)){
+    		
+    	} else {
+            write.println(input);
+    	}
     }
 }
